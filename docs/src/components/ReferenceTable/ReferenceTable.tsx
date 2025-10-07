b11810ea693d412eb7f9a9a736d4fc143ee9b5c1
dav-is <mail@connordav.is>
2025-10-07 15:07:16 -0400

    Fix headings

--- docs/src/components/ReferenceTable/ReferenceTable.tsx
+++ docs/src/components/ReferenceTable/ReferenceTable.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
-import kebabCase from 'es-toolkit/compat/kebabCase';
 import type { TypesContentProps } from '@mui/internal-docs-infra/abstractCreateTypes';
 import { useTypes } from '@mui/internal-docs-infra/useTypes';
 import { PropsReferenceAccordion } from './PropsReferenceAccordion';
 import { AttributesReferenceTable } from './AttributesReferenceTable';
 import { CssVariablesReferenceTable } from './CssVariablesReferenceTable';
-import { HeadingLink } from '../HeadingLink';
 
 type ReferenceTableProps = TypesContentProps<{
   asParam?: string;
+  hideDescription?: boolean;
 }>;
 
 export function ReferenceTable(props: ReferenceTableProps) {
@@ -19,26 +18,17 @@ export function ReferenceTable(props: ReferenceTableProps) {
     return null;
   }
 
-  const { asParam } = props;
+  const { asParam, hideDescription } = props;
   const data = type.data;
   const componentName = data.name;
-  const id = kebabCase(data.name);
 
   return (
     <React.Fragment>
       {/* Insert an <h3> with the part name for multi-part components.
           Single-part components headings and descriptions aren't displayed
           because they duplicate the page title and subtitle anyway. */}
-      {multiple && (
-        <React.Fragment>
-          <h3
-            className="mt-8 mb-1.5 scroll-mt-18 text-lg font-medium text-balance show-side-nav:scroll-mt-6"
-            id={id}
-          >
-            <HeadingLink id={id}>{data.name}</HeadingLink>
-          </h3>
-          {data.description && <p className="mb-4">{data.description}</p>}
-        </React.Fragment>
+      {multiple && !hideDescription && data.description && (
+        <p className="mb-4">{data.description}</p>
       )}
 
       {Object.keys(data.props).length > 0 && (
